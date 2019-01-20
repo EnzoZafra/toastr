249334f12a4af6f588f4868b16a31577d256d0a9
Mark Griffith <marky@Marks-MacBook-Pro.local>
2019-01-20 04:03:00 -0700

    Update toastr.py

--- toastr/toastr.py
+++ toastr/toastr.py
@@ -7,8 +7,8 @@ import sys
 IMAGE_DIR = "images/"
 TOASTR_BUCKET = "toastr_image"
 TOASTR_PROCESSED_BUCKET = "toastr_processedtext"
-AUTH_FILE = "../auth.json"
-PROCESSED_TEXT_DIR = "./upload_download/processed_text"
+AUTH_FILE = "../../auth.json"
+PROCESSED_TEXT_DIR = "processed_text"
 
 if __name__ == "__main__":
     # Execute script from script dir
@@ -26,5 +26,5 @@ if __name__ == "__main__":
         most_recent_img = max([os.path.join(IMAGE_DIR, basename) for basename in os.listdir(IMAGE_DIR)], key=os.path.getctime)
         upload.upload_file(most_recent_img, TOASTR_BUCKET)
         os.chdir(os.path.dirname(sys.argv[0])+"upload_download")
-        filename = most_recent_img + ".txt"
+        filename = os.path.basename(most_recent_img) + ".txt"
         download.download_object(TOASTR_PROCESSED_BUCKET, filename, PROCESSED_TEXT_DIR + "/" + filename, AUTH_FILE)
